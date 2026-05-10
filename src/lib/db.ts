b172555f0f46c7e484115374730326e612e7a72c
Z User <z@container>
2026-05-10 07:19:36 +0000

    Migrate to PostgreSQL for Vercel deployment

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,13 @@
 import { PrismaClient } from '@prisma/client'
 
-export const db = new PrismaClient({
-  log: ['error'],
-})
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
+
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['error'],
+  })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
